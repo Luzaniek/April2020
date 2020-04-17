7d1b8c1e307e48ba47f97678a5d1b0b79837f216
Piotr Luzny <piotr.luzny@delaval.com>
2020-04-17 11:20:41 +0200

    dr - part1

---
+++ april17.py
@@ -0,0 +1,15 @@
+print ("hej")
+
+
+class BinaryTree:
+    def __init__(self, number, left, right):
+        self.number = number
+        self.left = left
+        self.right = right
+
+    def getNumber(self):
+        return self.number
+
+
+class Start:
+    print ("sss")
